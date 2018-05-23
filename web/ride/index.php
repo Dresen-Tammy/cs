d3c1ca0edb1341ad363b2f72b6c722a1bc0f2147
Tammy Dresen <dre15003@byui.edu>
2018-05-23 16:40:19 -0700

    ugh

--- web/ride/index.php
+++ web/ride/index.php
@@ -1,41 +1,49 @@
 <?php
-include_once('../dbconnect.php');
+  include_once ('../../dbconnect.php');
 
+  $rideListQuery = $db->query('SELECT ride_name FROM public.ride;');
+  $rideListQuery = $db->execute();
+  $rideList = $rideListQuery->fetchAll(PDO::FETCH_NUM);
 
+  function flattenList($item) { return $item[0]; }
 
+  $rideList = array_map("flattenBook", $bookList);
+  ?>
 
+<!DOCTYPE html>
+<!-- Tammy Dresen, CS313,
+Login page where user logs in or creates new account
+Will include fields for username and password, login button, and register button. -->
 
-
-$bookListQuery = $db->query('SELECT DISTINCT rider_name FROM public.rider;');
-$bookListQuery->execute();
-$bookList = $bookListQuery->fetchAll(PDO::FETCH_NUM);
-
-function flattenBooks($item) { return $item[0]; }
-
-$bookList = array_map("flattenBooks", $bookList);
-?>
-
-<html>
+<html lang="en-us">
 <head>
-    <meta charset="utf-8">
+    <title>Ride Keeper Login</title>
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="./style.css">
-    <script src="./main.js"></script>
-    <title>Teach 05</title>
+    <link href="css/cart.css" type="text/css" rel="stylesheet">
 </head>
 <body>
-<h1>Scripture Resources</h1>
-<h2>Select a Book</h2>
-<select class="book-dropdown">
-    <?php
-    echo "<option value='' selected></option>";
-    foreach ($bookList as $book) {
-        echo "<option value='{$book}'>";
-        echo $book;
-        echo "</option>";
-    }
-    ?>
-</select>
-<ul class="scripture-list"></ul>
+<header>
+    <div class="center">
+        <img class="logo" src="images/recyclery.png" alt="recyclery logo">
+        <a href='index.php?action=logout' class='button1'>Log Out</a>"
+    </div>
+</header>
+<main>
+    <div class="center">
+        <h1>Ride Keeper</h1>
+        <h2>View Rides</h2>
+        <?php
+        foreach ($rideList as $ride) {
+            echo "<a href='rideDetail.php?item=$ride'>$ride</a>";
+        }
+        ?>
+        <a class="button1" href="index.php?action=byTrail">View By Trail</a>
+        <a class="button1" href="index.php?action=byDate">View By Date</a>
+        <a class="button1" href="index.php?action=thirtyDays">Last 30 Days</a>
+        <a class="button1" href="index.php?action=sevenDays">Last 7 Days</a>
+
+    </div>
+</main>
 </body>
 </html>
--- web/ride/view/viewRides.php
+++ web/ride/view/viewRides.php
@@ -1,5 +1,5 @@
 <?php
-$name = 'John';
+$name;
 ?>
 <!DOCTYPE html>
 <!-- Tammy Dresen, CS313,
@@ -11,12 +11,12 @@ Will include fields for username and password, login button, and register button
       <title>Ride Keeper Login</title>
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <link href="../css/cart.css" type="text/css" rel="stylesheet">
+      <link href="css/cart.css" type="text/css" rel="stylesheet">
   </head>
   <body>
   <header>
       <div class="center">
-          <img class="logo" src="../images/recyclery.png" alt="recyclery logo">
+          <img class="logo" src="images/recyclery.png" alt="recyclery logo">
           <a href='index.php?action=logout' class='button1'>Log Out</a>"
       </div>
   </header>
@@ -24,7 +24,14 @@ Will include fields for username and password, login button, and register button
       <div class="center">
           <h1>Ride Keeper</h1>
           <h2>View Rides</h2>
-              <?php echo $rideList ?>
+          <?php
+          echo "<option value='' selected></option>";
+          foreach ($bookList as $book) {
+              echo "<option value='{$book}'>";
+              echo $book;
+              echo "</option>";
+          }
+          ?>
           <a class="button1" href="index.php?action=byTrail">View By Trail</a>
           <a class="button1" href="index.php?action=byDate">View By Date</a>
           <a class="button1" href="index.php?action=thirtyDays">Last 30 Days</a>
