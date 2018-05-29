d3c349cc0308dc6b3dcc31dd53ef364f0985ede3
Tammy Dresen <dre15003@byui.edu>
2018-05-29 12:35:11 -0700

    css

--- web/team/wk6/index.php
+++ web/team/wk6/index.php
@@ -118,6 +118,7 @@ if (isset($_POST)) {
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="./style.css">
     <title>Teach 06</title>
 </head>
 <body>
