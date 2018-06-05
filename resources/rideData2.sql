b010b8e6e035553942627f729c436c469b06ac5c
Tammy Dresen <dre15003@byui.edu>
2018-06-04 18:19:27 -0700

    fix date

--- resources/rideData2.sql
+++ resources/rideData2.sql
@@ -5,7 +5,7 @@ INSERT INTO public.ride
 ,duration
 ,ride_name)
 VALUES
-(1
+(4
 ,1
 ,'2018-05-01'
 ,'1 hours 45 minutes'
@@ -19,7 +19,7 @@ INSERT INTO public.ride
 ,duration
 ,ride_name)
 VALUES
-(1
+(4
 ,2
 ,'2018-05-21'
 ,'2 hours 13 minutes'
@@ -33,7 +33,7 @@ INSERT INTO public.ride
 ,duration
 ,ride_name)
 VALUES
-(1
+(4
 ,3
 ,'2018-04-18'
 ,'2 hours 29 minutes'
@@ -47,7 +47,7 @@ INSERT INTO public.ride
 ,duration
 ,ride_name)
 VALUES
-(1
+(4
 ,2
 ,'2018-03-08'
 ,'1 hours 34 minutes'
@@ -61,7 +61,7 @@ INSERT INTO public.ride
 ,duration
 ,ride_name)
 VALUES
-(2
+(5
 ,3
 ,'2018-05-19'
 ,'1 hours 39 minutes'
@@ -75,7 +75,7 @@ INSERT INTO public.ride
 ,duration
 ,ride_name)
 VALUES
-(2
+(5
 ,3
 ,'2018-05-4'
 ,'1 hours 32 minutes'
